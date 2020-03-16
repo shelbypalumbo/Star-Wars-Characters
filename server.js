d140fe8a58b41cd83e8127a23166d2f17fbd85dc
shelby palumbo <shelbypalumbo94@gmail.com>
2020-03-16 11:00:23 -0400

    adding serve static files

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static('assets/css')); 
 // Star Wars Characters (DATA)
 // =============================================================
 var characters = [
