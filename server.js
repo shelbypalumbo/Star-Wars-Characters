b9385f1d6ed239e3d9283f2ee0f7d1309f0eb557
shelby palumbo <shelbypalumbo94@gmail.com>
2020-03-16 21:06:06 -0400

    static files

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('assets/css')); 
+app.use(express.static('assets/css/style.css')); 
+app.use(express.static('assets/css/images')); 
 // Star Wars Characters (DATA)
 // =============================================================
 var characters = [
